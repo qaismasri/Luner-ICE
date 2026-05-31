aec48a3ecfaf9134f4e6add2fe63cc6f3250831e
Shivang Mehra <shivangmehra2008@gmail.com>
2026-05-31 19:29:26 +0100

    Added Radio test code Added Radio + Ultrasound + Magnetic test code Added website + RUM code.

---
+++ sensing/radio/radio_test_code/src/main.cpp
@@ -0,0 +1,37 @@
+#include <Arduino.h>
+
+String message = "";
+
+void setup() {
+  Serial.begin(115200);
+  Serial1.begin(600);
+  Serial.println("Started");
+}
+
+void loop() {
+  if (Serial1.available()) {
+    char c = Serial1.read();
+    
+    // Print ASCII code number AND the character
+    Serial.print("[");
+    Serial.print(c);
+    Serial.print("=");
+    Serial.print((int)c);  // prints the ASCII number
+    Serial.print("]");
+    
+    if (c == '#') {
+      Serial.println(" <-- got header!");
+      message = "";
+    } else {
+      message += c;
+      if (message.length() == 3) {
+        int age = message.toInt();
+        float ageInBillions = age / 100.0;
+        Serial.print("  ->  Rock age: ");
+        Serial.print(ageInBillions);
+        Serial.println(" billion years");
+        message = "";
+      }
+    }
+  }
+}
